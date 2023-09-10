a896c7c52e5ff60389bca05259e9dcf67cc21516
vighnesh-sablok <92384195+vighnesh-sablok@users.noreply.github.com>
2023-09-10 22:35:27 +0530

    Update train.py

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
 
     # Split the data into training and test sets. (0.75, 0.25) split.
     train, test = train_test_split(data)
-
+    print(mlflow.get_tracking_uri())
     # The predicted column is "quality" which is a scalar from [3, 9]
     train_x = train.drop(["quality"], axis=1)
     test_x = test.drop(["quality"], axis=1)
